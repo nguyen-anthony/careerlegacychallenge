63851be0df5ce27bfa83d4eae78f17fa74ceadcd
Tony <53980868+nguyen-anthony@users.noreply.github.com>
2022-03-13 18:52:16 -0500

    fix my project (#2)

--- careerlegacy.js
+++ careerlegacy.js
@@ -12,10 +12,6 @@ randomizeBtn.addEventListener("click", () => {
     'input[class="subOption"]:checked'
   );
 
-  if (listOfAvailableCareers.length == 0) {
-    selectedCareer.innerHTML = "No careers selected!";
-  }
-
   var randomize = setInterval(function () {
     var random = Math.floor(Math.random() * listOfAvailableCareers.length);
     selectedCareer.innerHTML = listOfAvailableCareers[random].name;
@@ -53,13 +49,12 @@ checkall.forEach(function (check) {
   }
 
   check.onclick = function () {
+<<<<<<< HEAD
+=======
+    var subOptions = document.querySelectorAll('#' + CSS.escape(check.id) + '+label+ul input');
+>>>>>>> parent of 16c694e (added accordian checkboxes)
     for (var i = 0; i < subOptions.length; i++) {
       subOptions[i].checked = this.checked;
-    }
-    if(subOptionList.style.display === "none"){
-      subOptionList.style.display = "block";
-    } else {
-      subOptionList.style.display = "none";
     }
   }
 });
