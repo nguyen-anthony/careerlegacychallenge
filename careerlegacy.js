d0d55a211602f6c51028bf335cc6467476d2fc76
Tony Nguyen <nguyen.tonyphuc@gmail.com>
2022-03-31 00:58:50 -0500

    updated js to obtain the randomized career

--- careerlegacy.js
+++ careerlegacy.js
@@ -4,9 +4,9 @@ const randomizeBtn = document.getElementById("randomizeCareerBtn");
 const selectedCareer = document.getElementById("careerchoice");
 
 /*
-* Randomize button functionality.
-* Repeatedly randomizes a career choice based on what suboptions are checked for 1.5 seconds and updates the output
-*/
+ * Randomize button functionality.
+ * Repeatedly randomizes a career choice based on what suboptions are checked for 1.5 seconds and updates the output
+ */
 randomizeBtn.addEventListener("click", () => {
   var listOfAvailableCareers = document.querySelectorAll(
     'input[class="subOption"]:checked'
@@ -23,43 +23,53 @@ randomizeBtn.addEventListener("click", () => {
 
   setTimeout(function () {
     clearInterval(randomize);
+    console.log(selectedCareer.innerHTML);
   }, 1500);
+
+  // setTimeout(console.log(selectedCareer.innerHTML), 3000);
 });
 
 //Get all game pack checkboxes
-var checkall = document.querySelectorAll('.packCheckBox');
+var checkall = document.querySelectorAll(".packCheckBox");
 
 /*
-* Two parts:
-* 1. Sets indeterminate, checked, or unchecked based on number of subOptions are checked
-* 2. Check all subOptions if game pack option is checked
-*/
+ * Two parts:
+ * 1. Sets indeterminate, checked, or unchecked based on number of subOptions are checked
+ * 2. Check all subOptions if game pack option is checked
+ */
 checkall.forEach(function (check) {
-  var subOptions = document.querySelectorAll('#' + CSS.escape(check.id) + '+label+ul input');
-  var subOptionList = document.querySelector('#' + CSS.escape(check.id) + '+label+ul');
+  var subOptions = document.querySelectorAll(
+    "#" + CSS.escape(check.id) + "+label+ul input"
+  );
+  var subOptionList = document.querySelector(
+    "#" + CSS.escape(check.id) + "+label+ul"
+  );
 
   for (var i = 0; i < subOptions.length; i++) {
     subOptions[i].onclick = function () {
-      var checkedCount = document.querySelectorAll('#' + CSS.escape(check.id) + '+label+ul input:checked').length;
+      var checkedCount = document.querySelectorAll(
+        "#" + CSS.escape(check.id) + "+label+ul input:checked"
+      ).length;
 
       check.checked = checkedCount > 0;
-      check.indeterminate = checkedCount > 0 && checkedCount < subOptions.length;
-      if(checkedCount == 0){
+      check.indeterminate =
+        checkedCount > 0 && checkedCount < subOptions.length;
+      if (checkedCount == 0) {
         subOptionList.style.display = "none";
       } else {
         subOptionList.style.display = "block";
       }
-    }
+    };
   }
 
   check.onclick = function () {
     for (var i = 0; i < subOptions.length; i++) {
       subOptions[i].checked = this.checked;
     }
-    if(subOptionList.style.display === "none"){
+    if (subOptionList.style.display === "none") {
       subOptionList.style.display = "block";
     } else {
       subOptionList.style.display = "none";
     }
-  }
+  };
 });
