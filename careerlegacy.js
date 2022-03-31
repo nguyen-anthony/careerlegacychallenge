4f4a719a94a6738d35ede15b7cbe53cc64819e74
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-03-31 11:46:39 -0500

    updated js to account for no careers selected

--- careerlegacy.js
+++ careerlegacy.js
@@ -3,6 +3,9 @@ var parentCheckBoxes = document.querySelectorAll(".packCheckBox");
 const randomizeBtn = document.getElementById("randomizeCareerBtn");
 const selectedCareer = document.getElementById("careerchoice");
 
+//Get all game pack checkboxes
+var checkall = document.querySelectorAll(".packCheckBox");
+
 /*
  * Randomize button functionality.
  * Repeatedly randomizes a career choice based on what suboptions are checked for 1.5 seconds and updates the output
@@ -14,36 +17,34 @@ randomizeBtn.addEventListener("click", () => {
 
   if (listOfAvailableCareers.length == 0) {
     selectedCareer.innerHTML = "No careers selected!";
-  }
+    alert("Please select some careers in the list!");
+  } else {
+    var randomize = setInterval(function () {
+      var random = Math.floor(Math.random() * listOfAvailableCareers.length);
+      selectedCareer.innerHTML = listOfAvailableCareers[random].name;
+    }, 10);
 
-  var randomize = setInterval(function () {
-    var random = Math.floor(Math.random() * listOfAvailableCareers.length);
-    selectedCareer.innerHTML = listOfAvailableCareers[random].name;
-  }, 10);
-
-  setTimeout(function () {
-    clearInterval(randomize);
-    console.log(selectedCareer.innerHTML);
     setTimeout(function () {
-      if (
-        confirm(
-          "Do you want to play the " + selectedCareer.innerHTML + " career?"
-        )
-      ) {
-        $("#completedList").append("<li>" + selectedCareer.innerHTML + "</li>");
-        let inputCheckbox = document.querySelector(
-          'input[name="' + selectedCareer.innerHTML + '"]'
-        );
-        inputCheckbox.checked = false;
-      }
-    }, 300);
-  }, 1500);
+      clearInterval(randomize);
 
-  // setTimeout(console.log(selectedCareer.innerHTML), 3000);
+      setTimeout(function () {
+        if (
+          confirm(
+            "Do you want to play the " + selectedCareer.innerHTML + " career?"
+          )
+        ) {
+          $("#completedList").append(
+            "<li>" + selectedCareer.innerHTML + "</li>"
+          );
+          let inputCheckbox = document.querySelector(
+            'input[name="' + selectedCareer.innerHTML + '"]'
+          );
+          inputCheckbox.checked = false;
+        }
+      }, 300);
+    }, 1500);
+  }
 });
-
-//Get all game pack checkboxes
-var checkall = document.querySelectorAll(".packCheckBox");
 
 /*
  * Two parts:
