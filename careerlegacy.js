158f4ac852f6d0e39231960e9229cea0d9797c96
Tony Nguyen <nguyen.tonyphuc@gmail.com>
2022-04-13 00:26:37 -0500

    update checkbox logic

--- careerlegacy.js
+++ careerlegacy.js
@@ -48,6 +48,7 @@ randomizeBtn.addEventListener("click", () => {
           inputCheckbox.checked = false;
           inputCheckbox.disabled = true;
         }
+        checkParent();
       }, 300);
     }, 1500);
   }
@@ -94,3 +95,29 @@ checkall.forEach(function (check) {
     }
   };
 });
+
+function checkParent() {
+  checkall.forEach(function (check) {
+    var subOptions = document.querySelectorAll(
+      "#" + CSS.escape(check.id) + "+label+ul input"
+    );
+    var subOptionList = document.querySelector(
+      "#" + CSS.escape(check.id) + "+label+ul"
+    );
+
+    for (var i = 0; i < subOptions.length; i++) {
+      var checkedCount = document.querySelectorAll(
+        "#" + CSS.escape(check.id) + "+label+ul input:checked"
+      ).length;
+
+      check.checked = checkedCount > 0;
+      check.indeterminate =
+        checkedCount > 0 && checkedCount < subOptions.length;
+      if (checkedCount == 0) {
+        subOptionList.style.display = "none";
+      } else {
+        subOptionList.style.display = "block";
+      }
+    }
+  });
+}
